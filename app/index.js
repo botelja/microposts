0212a1e189939f6bc2dfa6d64b15030320dc0491
botelja <ivo.surjak@gmail.com>
2019-09-08 13:14:51 +0200

    Add posts and show alert

--- app/index.js
+++ app/index.js
@@ -5,9 +5,34 @@ import './main.css';
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for submit post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Get posts
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
     .then((data) => ui.showPosts(data))
+    .catch((err) => console.log(err));
+}
+
+// Submit post
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body
+  };
+
+  // Create post
+  http
+    .post('http://localhost:3000/posts', data)
+    .then((data) => {
+      ui.showAlert('Post added', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
     .catch((err) => console.log(err));
 }
--- app/ui.js
+++ app/ui.js
@@ -1,15 +1,18 @@
-function UI() {}
+function UI() {
+  this.post = document.querySelector('#posts');
+  this.postTitle = document.querySelector('#title');
+  this.postBody = document.querySelector('#body');
+}
 
-UI.prototype.showPosts = (posts) => {
-  const post = document.querySelector('#posts');
+UI.prototype.showPosts = function(posts) {
   let output = '';
 
   posts.forEach((post) => {
     output += `
           <div class="card mb-3">
               <div class="card-body">
-                  <h4 class="card-title>${post.title}</h4>
-                  <p class="card-text>${post.body}</p>
+                  <h4 class="card-title">${post.title}</h4>
+                  <p class="card-text">${post.body}</p>
                   <a href="#" class="edit card-link" data-id="${post.id}">
                     <i class="fa fa-pencil"></i>
                   </a>
@@ -20,7 +23,42 @@ UI.prototype.showPosts = (posts) => {
           </div>
         `;
   });
-  return (post.innerHTML = output);
+  this.post.innerHTML = output;
+};
+
+UI.prototype.showAlert = function(message, className) {
+  this.clearAlert();
+
+  // Create a div element
+  const div = document.createElement('div');
+  // Add classes
+  div.className = className;
+  // Add text
+  div.appendChild(document.createTextNode(message));
+  // Get parent
+  const container = document.querySelector('.postsContainer');
+  // Get posts
+  const posts = document.querySelector('#posts');
+  // Insert alert div
+  container.insertBefore(div, posts);
+
+  //Timeout
+  setTimeout(() => {
+    this.clearAlert();
+  }, 3000);
+};
+
+UI.prototype.clearAlert = function() {
+  const currentAlert = document.querySelector('.alert');
+
+  if (currentAlert) {
+    currentAlert.remove();
+  }
+};
+
+UI.prototype.clearFields = function() {
+  this.postTitle.value = '';
+  this.postBody.value = '';
 };
 
 export const ui = new UI();
