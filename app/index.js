1811899a0bf396f3c7ad0c7cb332ee9372f5b2c6
botelja <ivo.surjak@gmail.com>
2019-09-08 17:57:10 +0200

    Remove post

--- app/index.js
+++ app/index.js
@@ -8,6 +8,9 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // Listen for submit post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for delete post
+document.querySelector('#posts').addEventListener('click', deletePost);
+
 // Get posts
 function getPosts() {
   http
@@ -35,4 +38,21 @@ function submitPost() {
       getPosts();
     })
     .catch((err) => console.log(err));
+}
+
+// Delete post
+function deletePost(e) {
+  e.preventDefault();
+  if (e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if (confirm('Are you sure?')) {
+      http
+        .delete(`http://localhost:3000/posts/${id}`)
+        .then((data) => {
+          ui.showAlert('Post Removed', 'alert alert-success');
+          getPosts();
+        })
+        .catch((err) => console.log(err));
+    }
+  }
 }
